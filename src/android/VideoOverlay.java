b6b5f3b78e794011bf82f1f201200d0af3ffd59d
Adam Clarke <oreo@throwawaygames.ca>
2015-11-01 15:25:45 -0500

    trimmed down rotation function

--- src/android/VideoOverlay.java
+++ src/android/VideoOverlay.java
@@ -74,7 +74,7 @@ public class VideoOverlay extends ViewGroup {
         camera.unlock();
 
         try {
-            setCameraDisplayOrientation(this,0,camera);
+            setCameraDisplayOrientation(0,camera);
 
             recorder = new MediaRecorder();
             recorder.setCamera(camera);
@@ -141,30 +141,17 @@ public class VideoOverlay extends ViewGroup {
     }
 
 
-    public static void setCameraDisplayOrientation(Activity activity, int cameraId, android.hardware.Camera camera) {
+    public static void setCameraDisplayOrientation(int cameraId, android.hardware.Camera camera) {
 
         android.hardware.Camera.CameraInfo info =
             new android.hardware.Camera.CameraInfo();
 
         android.hardware.Camera.getCameraInfo(cameraId, info);
 
-        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
-        int degrees = 0;
-
-        switch (rotation) {
-            case Surface.ROTATION_0: degrees = 0; break;
-            case Surface.ROTATION_90: degrees = 90; break;
-            case Surface.ROTATION_180: degrees = 180; break;
-            case Surface.ROTATION_270: degrees = 270; break;
-        }
+        int degrees = 90;
 
         int result;
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-            result = (info.orientation + degrees) % 360;
-            result = (360 - result) % 360;  // compensate the mirror
-        } else {  // back-facing
-            result = (info.orientation - degrees + 360) % 360;
-        }
+        result = (info.orientation - degrees + 360) % 360;
         VideoWithSurfaceVw.orientation=result;
         camera.setDisplayOrientation(result);
     }
