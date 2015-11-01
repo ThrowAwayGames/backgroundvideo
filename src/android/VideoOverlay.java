8bc02c2a6840fc311aeb8c763dbf69dfd78decbe
Adam Clarke <oreo@throwawaygames.ca>
2015-11-01 15:37:13 -0500

    rotation tests

--- src/android/VideoOverlay.java
+++ src/android/VideoOverlay.java
@@ -182,7 +182,7 @@ public class VideoOverlay extends ViewGroup {
         }
 
         camera.setParameters(parameters);
-        //camera.setDisplayOrientation(90);
+        camera.setDisplayOrientation(90);
     }
 
     private void setProfile(MediaRecorder mediaRecorder, Camera.Parameters parameters){
